effbf4b6e5cd05dd97d7e55c23445634bc86d0df
ethereal element <vitalik.safonov@list.ru>
2023-11-05 14:46:15 +0300

    first sol task3

---
+++ task3.js
@@ -0,0 +1,11 @@
+
+const nums = [3,0,1];
+const missingNumber = (nums) => {
+    const length = nums.length;
+
+    for (let i = 0; i <= nums.length - 1; i++) {
+        if (nums[i] === length) {
+
+        }
+    }
+}
