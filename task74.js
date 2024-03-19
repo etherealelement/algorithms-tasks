2e3723a8bdb399fa3ad3ba9564a356cd66cb6407
Vitaliy Safonov <vitaliysafonov@livedune.com>
2024-03-19 16:04:02 +0300

    sol task task74.js

---
+++ task74.js
@@ -0,0 +1,8 @@
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+    const sortS = s.split("").sort().join(" ");
+    const sortT = t.split("").sort().join(" ");
+    return sortS === sortT
+};
+
+console.log(isAnagram("anagram", "nagaram"))
