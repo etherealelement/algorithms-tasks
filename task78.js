5226ec3e3113a0ca76ee7051d12070d19153ac80
Vitaliy Safonov <vitaliysafonov@livedune.com>
2024-03-24 20:41:21 +0300

    first sol task78

---
+++ task78.js
@@ -0,0 +1,16 @@
+var isPalindrome = function(s) {
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let start = 0,
+     end = s.length - 1;
+
+    while (start <= end) {
+        if (s[start] !== s[end]) {
+            return false
+        }
+        start++
+        end--;
+    }
+    return true;
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"))
