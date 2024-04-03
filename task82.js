cca90f41c50f9137a104f18e39732c287f39de24
Vitaliy Safonov <vitaliysafonov@livedune.com>
2024-04-03 18:04:43 +0300

    first sol task82

---
+++ task82.js
@@ -0,0 +1,18 @@
+var isValid = function(s) {
+    const stack = [];
+    const brackets = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (brackets[char]) {
+            stack.push(char);
+        } else if (char !== brackets[stack.pop()]) {
+            return false;
+        }
+    }
+    return stack.length === 0;
+};
+console.log(isValid("()[]{}"))
